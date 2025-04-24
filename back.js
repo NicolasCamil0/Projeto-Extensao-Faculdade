78cfff97d5d0dc5f193645e353e279e65fb82fd4
Nicolas I Camilo Silva <130256661+MiioCh@users.noreply.github.com>
2025-04-24 20:59:00 -0300

    hot fix back

--- back.js
+++ back.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const button = document.querySelector(".inputArea button"); 
     
     
+    const apiBase = window.location.hostname.includes("localhost")
+        ? "http://localhost:8080"
+        : "https://projeto-extensao-faculdade.onrender.com";
+
     button.addEventListener("click", function () {
         const texto = input.value.trim();
         if (texto === "") return; 
 
-       
         adicionarMensagem(texto, "userMessage");
 
-        // Envia a mensagem ao backend
-        fetch(`http://localhost:8080/excel/pergunta?texto=${encodeURIComponent(texto)}`)
+        
+        fetch(`${apiBase}/excel/pergunta?texto=${encodeURIComponent(texto)}`)
             .then(response => response.text())
             .then(resposta => {
-                
                 adicionarMensagem(resposta, "botMessage");
             })
             .catch(error => {
@@ -26,29 +28,21 @@ document.addEventListener("DOMContentLoaded", function () {
         input.value = ""; 
     });
 
-   
     function adicionarMensagem(mensagem, classe) {
         const divMensagem = document.createElement("div");
         divMensagem.classList.add("message", classe);
-        
-       
         divMensagem.innerHTML = converterMarkdownParaHTML(mensagem); 
-
         chatContainer.appendChild(divMensagem);
-
-       
         chatContainer.scrollTop = chatContainer.scrollHeight;
     }
 
-    
     function converterMarkdownParaHTML(texto) {
         return texto.replace(/\n/g, '<br>');
     }
 
-   
     input.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
-            button.click(); // 
+            button.click(); 
         }
     });
 });
