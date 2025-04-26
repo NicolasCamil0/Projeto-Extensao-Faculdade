f1fe388c1a6fcd0a3af85b7a9566b0dea96ffa47
Nicolas I Camilo Silva <130256661+MiioCh@users.noreply.github.com>
2025-04-26 15:28:46 -0300

    cors corrigido

--- src/main/java/org/example/Controller/Cors.java
+++ src/main/java/org/example/Controller/Cors.java
@@ -1,8 +1,10 @@
 package org.example.Controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class Cors implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permite CORS em todas as rotas
